9ae5c2681dfb8c0c8a2b782509d3deb8c8b56658
Gajendrsingh <80196896+Gajendrsingh@users.noreply.github.com>
2021-06-27 14:04:14 +0530

    Add files via upload

---
+++ website.sh
@@ -0,0 +1,89 @@
+#!bin/bash
+RED="\e[92m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED}         ___--=--------___
+        /. \___\____   _, \_      /-\
+       /. .  _______     __/=====@
+       \----/  |  / \______/      \-/
+   _/         _/ o \
+  / |    o   /  ___ \
+ / /    o\\ |  / O \ /|      __-_
+|o|     o\\\   |  \ \ /__--o/o___-_
+| |      \\\-_  \____  ----  o___-
+
+|o|       \_ \     /\______-o\_-
+| \       _\ \  _/ / |
+\o \_   _/      __/ /
+ \   \-/   _       /|_
+  \_      / |   - \  |\
+    \____/  \ | /  \   |\
+            | o |   | \ |
+            | | |    \ | \
+           / | /      \ \ \
+         /|  \o|\--\  /  o |\--\
+         \----------' \---------'
+contact me my WhatsApp number 
+https://wa.me/+19095822230
+https://wa.me/+15034555318
+https://t.me/joinchat/v1UBTdiM1ZZhNjdh
+https://youtube.com/channel/UCLGRNqwl7y-fXT3IXX9AveAcontact me my WhatsApp number 
+https://wa.me/+19095822230
+https://wa.me/+15034555318
+https://t.me/joinchat/v1UBTdiM1ZZhNjdh
+https://youtube.com/channel/UCLGRNqwl7y-fXT3IXX9AveA
+    
+
+
+${ENDCOLOR}"
+
+
+
+
+
+
+
+
+
+
+
+
+sudo apt install amass dmitry dnsrecon wapiti  dnsenum  dirb wafw00f lbd xsser dnsmap dnswalk fierce  davtest whatweb uniscan whois python2 nmap -y
+apt install amass dmitry dnsrecon wapiti  dnsenum  dirb wafw00f lbd xsser dnsmap dnswalk fierce  davtest whatweb uniscan whois python2 nmap -y
+
+echo -e "\e[32m      ___--=--------___
+        /. \___\____   _, \_      /-\
+       /. .  _______     __/=====@
+       \----/  |  / \______/      \-/
+   _/         _/ o \
+  / |    o   /  ___ \
+ / /    o\\ |  / O \ /|      __-_
+|o|     o\\\   |  \ \ /__--o/o___-_
+| |      \\\-_  \____  ----  o___-
+
+|o|       \_ \     /\______-o\_-
+| \       _\ \  _/ / |
+\o \_   _/      __/ /
+ \   \-/   _       /|_
+  \_      / |   - \  |\
+    \____/  \ | /  \   |\
+            | o |   | \ |
+            | | |    \ | \
+           / | /      \ \ \
+         /|  \o|\--\  /  o |\--\
+         \----------' \---------'
+contact me my WhatsApp number 
+https://wa.me/+19095822230
+https://wa.me/+15034555318
+https://t.me/joinchat/v1UBTdiM1ZZhNjdh
+https://youtube.com/channel/UCLGRNqwl7y-fXT3IXX9AveAcontact me my WhatsApp number 
+
+
+i am kelvin her i am from Illuminati world secret society
+
+
+
+
+enter website name want you scan :\e[0m"
+read a
+python2 rapidscan.py $a
